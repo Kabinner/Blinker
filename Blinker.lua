2ac2fb247085ac28dc9f227029837762668cfe39
Kabinner <xeddicuzwow@gmail.com>
2025-02-06 20:05:42 +0100

    Bugfixes

--- Blinker.lua
+++ Blinker.lua
@@ -35,10 +35,10 @@ local function SpellReady(spellname)
 end
 
 local function Status()
-    if _unitxp_loaded then
+    if _unitxp_loaded == true then
         local txt = ""
         local n = UnitXP("timer", "size");
-        if _timer_id then
+        if _timer_id => 0 then
             txt = "id: " .. _timer_id;
         end
         Print("Status:" .. txt .. " Running: #" .. n);
@@ -66,14 +66,14 @@ function Blinker()
 end
 
 function Blinker_Enable()
-    if _timer_id and _debug then
+    if _timer_id => 0 and _debug == true then
         Status()
         return;
     end
 
-    if _unitxp_loaded then
+    if _unitxp_loaded == true then
         _timer_id = UnitXP("timer", "arm", 100, 100, "Blinker");
-        if _debug then
+        if _debug == true then
             Print("Blinker_Enable: id=" .. _timer_id);
             Status()
         end
@@ -91,12 +91,12 @@ function Blinker_Enable()
     Print("is enabled.")
 end
 function Blinker_Disable()
-    if _timer_id then
+    if _timer_id => 0 then
         Print("Blinker_Disabled: id=" .. _timer_id);
         UnitXP("timer", "disarm", _timer_id);
         _timer_id = nil;
     end
-    if _debug then
+    if _debug == true then
         Status()
     end
     Print("is disabled.")
@@ -114,7 +114,7 @@ function Blinker_OnEvent(event)
         this:UnregisterEvent("ADDON_LOADED");
     elseif event == "PLAYER_LOGIN" then
         if not _unitxp_loaded then
-            if _debug then
+            if _debug == true then
                 Print("Error: UnitXP is not loaded.");
             end
             return;
