48b20744a8813b51650e69b4b3c6afa7ffd9a29b
Kabinner <david.bisschops@gmx.com>
2025-02-08 12:51:22 +0100

    Added Minimap & Settings

--- Blinker.lua
+++ Blinker.lua
@@ -1,6 +1,59 @@
-local _unitxp_loaded, _timer_id, _prev_x, _prev_y, _last_spell_cast;
-local _last_update_time = 0;
 local _debug = false;
+local _is_active
+local _unitxp_loaded, _timer_id, _timer_button_move_id, _timer_tooltip_id
+local _prev_x, _prev_y, _last_spell_cast;
+local _last_update_time = 0;
+local TooltipFrame, MinimapFrame, MinimapButtonFrame;
+
+local function print_r ( t ) 
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if type(t)=="table" then
+                for i in t do
+                    local val = t[i]
+                    if (type(val)=="table") then
+                        print(indent.."#["..i.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(i)+8))
+                        print(indent..string.rep(" ",string.len(i)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."#["..i..'] => "'..val..'"')
+                    else
+                        print(indent.."#["..i.."] => "..tostring(val))
+                    end
+                end
+                for pos,val in pairs(t) do
+                    if type(pos) ~= "number" or math.floor(pos) ~= pos or (pos < 1 or pos > tLen) then
+                        if (type(val)=="table") then
+                            print(indent.."["..pos.."] => "..tostring(t).." {")
+                            sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                            print(indent..string.rep(" ",string.len(pos)+6).."}")
+                        elseif (type(val)=="string") then
+                            print(indent.."["..pos..'] => "'..val..'"')
+                        else
+                            print(indent.."["..pos.."] => "..tostring(val))
+                        end
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+        
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+
+    print()
+end
 
 local function Print(msg)
     if not DEFAULT_CHAT_FRAME or not msg then
@@ -14,8 +67,8 @@ local function SpellId(spellname)
     for i = 1, GetNumSpellTabs() do
         local _, _, _, numSpells = GetSpellTabInfo(i);
         for j = 1, numSpells do
-            local spellName = GetSpellName(id, BOOKTYPE_SPELL);
-            if spellName == spellname then
+            local _spell_name = GetSpellName(id, BOOKTYPE_SPELL);
+            if _spell_name == spellname then
                 return id;
             end
             id = id + 1;
@@ -57,11 +110,103 @@ local function IsMoving(x, y)
     return false;
 end
 
+function TooltipFrame_Show(parent, text, r, g, b)
+    if _timer_tooltip_id then
+        UnitXP("timer", "disarm", _timer_tooltip_id)
+    end
+
+    anchor = "ANCHOR_CURSOR"
+    timeout = 3000
+    if parent ~= UIParent then
+        anchor = "ANCHOR_LEFT"
+        timeout = 1000
+    end
+
+    TooltipFrame:SetOwner(parent, anchor, 0, 0)
+    TooltipFrame:SetText("[Blinker] " .. text, r, g, b)
+    TooltipFrame:Show()
+
+    _timer_tooltip_id = UnitXP("timer", "arm", timeout, 0, "TooltipFrame_Hide");
+end
+function TooltipFrame_Hide()
+    TooltipFrame:Hide()
+end
+function MinimapButtonFrame_OnMove()
+    if IsShiftKeyDown() then return end
+
+    MinimapButtonFrame:StopMovingOrSizing()
+    UnitXP("timer", "disarm", _timer_button_move_id)
+end
+function Blinker_UI()
+    StaticPopupDialogs["BLINKER_UI_FRAME_DIALOG_SPELL"] = {
+        text = "Enter spell name:",
+        hasEditBox = true,
+        OnAccept = function(self)
+            if SpellId(StaticPopup1EditBox:GetText()) then
+                Blinker_Settings.spell_name = StaticPopup1EditBox:GetText()
+                TooltipFrame_Show(UIParent, "Successfully set to: " .. Blinker_Settings.spell_name)
+            else
+                TooltipFrame_Show(UIParent, "Error: " .. StaticPopup1EditBox:GetText() .. " does not exist.", 1.0, 0.0, 0.0)
+            end
+        end,
+        OnCancel = function()
+            StaticPopup_Hide("BLINKER_UI_FRAME_DIALOG_SPELL")
+        end,
+        button1 = ACCEPT,
+        button2 = CANCEL,
+        maxLetters = 50,
+        timeout = 0,
+        whileDead = true,
+        hideOnEscape = true,
+    }
+
+    TooltipFrame = CreateFrame("GameTooltip", "BLINKER_UI_FRAME_TOOLTIP", UIParent, "GameTooltipTemplate")
+
+    MinimapButtonFrame = CreateFrame("Button", "BLINKER_UI_FRAME_MINIMAP_BUTTON", Minimap)
+    MinimapButtonFrame:SetWidth(24)  
+    MinimapButtonFrame:SetHeight(24)  
+    MinimapButtonFrame:SetFrameStrata("MEDIUM")
+    MinimapButtonFrame:SetMovable(true)
+    MinimapButtonFrame:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 0, 0)
+	MinimapButtonFrame:EnableMouse(true)
+    MinimapButtonFrame:RegisterForClicks("LeftButtonDown", "RightButtonDown");
+    MinimapButtonFrame:SetNormalTexture("Interface\\Icons\\Spell_Arcane_Blink")
+    MinimapButtonFrame:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
+    MinimapButtonFrame:SetScript("OnEnter", function(self, event)
+        local str = ""
+        if _timer_id then str = str .. "active" else str = str .. "inactive" end
+
+        TooltipFrame_Show(MinimapButtonFrame, str .. ": " .. Blinker_Settings.spell_name)
+    end)
+
+    MinimapButtonFrame:SetScript("OnClick", function(self)
+        if IsShiftKeyDown() then
+            if _timer_button_move_id then
+                UnitXP("timer", "disarm", _timer_button_move_id)
+            end
+            this:StartMoving()
+            _timer_button_move_id = UnitXP("timer", "arm", 200, 200, "MinimapButtonFrame_OnMove");
+
+        elseif arg1 == "LeftButton" then
+            if _timer_id then
+                Blinker_Disable()
+                TooltipFrame_Show(MinimapButtonFrame, Blinker_Settings.spell_name .. " is disabled.")
+            else
+                Blinker_Enable()
+                TooltipFrame_Show(MinimapButtonFrame, Blinker_Settings.spell_name .. " is enabled.")
+            end
+        elseif arg1 == "RightButton" then
+            StaticPopup_Show("BLINKER_UI_FRAME_DIALOG_SPELL")
+        end
+    end)
+end
+
+
 function Blinker()
     local x, y = GetPlayerMapPosition("player");
-    if IsMoving(x, y) and SpellReady("Blink") and IsShiftKeyDown() then
-        CastSpellByName("Blink");
-        _last_spell_cast = GetTime();
+    if IsMoving(x, y) and SpellReady(Blinker_Settings.spell_name) and IsShiftKeyDown() then
+        CastSpellByName(Blinker_Settings.spell_name);
+        _last_spell_cast = GetTime()
     end
 end
 
@@ -70,15 +215,22 @@ function Blinker_Enable()
         Print("cannot load. Dependency UnitXP_SP3 not found. [https://github.com/allfoxwy/UnitXP_SP3].");
         return;
     end
+
+    if not Blinker_Settings then
+		Blinker_Settings = {
+			spell_name = "Blink",
+		}
+	end
+
     if _timer_id and _debug then
-        Status();
+        Status()
     end
 
-    _last_spell_cast = GetTime();
+    _last_spell_cast = GetTime()
     _timer_id = UnitXP("timer", "arm", 100, 100, "Blinker");
     if _debug then
         Print("Blinker_Enable: id=" .. _timer_id);
-        Status();
+        Status()
     end
     Print("is enabled.")
 end
@@ -90,28 +242,29 @@ function Blinker_Disable()
         return;
     end
     if _timer_id then
-        Print("Blinker_Disabled: id=" .. _timer_id);
-        UnitXP("timer", "disarm", _timer_id);
+        if _debug then
+            Print("Blinker_Disabled: id=" .. _timer_id)
+            Status()
+        end
+        UnitXP("timer", "disarm", _timer_id)
         _timer_id = nil;
-    end
-    if _debug then
-        Status();
     end
     Print("is disabled.")
 end
 function Blinker_OnLoad()
     this:RegisterEvent("ADDON_LOADED")
-    this:RegisterEvent("PLAYER_LOGIN");
-    this:RegisterEvent("PLAYER_LOGOUT");
+    this:RegisterEvent("PLAYER_LOGIN")
+    this:RegisterEvent("PLAYER_LOGOUT")
 end
 function Blinker_OnEvent(event)
     if event == "ADDON_LOADED" and arg1 == "Blinker" then
         if pcall(UnitXP, "nop", "nop") then
-            _unitxp_loaded = true;
+            _unitxp_loaded = true
         end
-        this:UnregisterEvent("ADDON_LOADED");
+        this:UnregisterEvent("ADDON_LOADED")
     elseif event == "PLAYER_LOGIN" then
         Blinker_Enable()
+        Blinker_UI()
     elseif event == "PLAYER_LOGOUT" then
         Blinker_Disable()
     end
