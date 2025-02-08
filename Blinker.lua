16739f063eaf54bdc65e9393f55bcabd9b0a36b8
Kabinner <david.bisschops@gmx.com>
2025-02-08 18:24:53 +0100

    Refactoring into OOP

--- Blinker.lua
+++ Blinker.lua
@@ -1,67 +1,17 @@
 local _debug = false;
-local _is_active
 local _unitxp_loaded, _timer_id, _timer_button_move_id, _timer_tooltip_id
 local _prev_x, _prev_y, _last_spell_cast
 local _last_update_time = 0
 local TooltipFrame, MinimapFrame, MinimapButtonFrame
 
-local function print_r(t)
-    local print_r_cache = {}
-    local function sub_print_r(t, indent)
-        if (print_r_cache[tostring(t)]) then
-            print(indent .. "*" .. tostring(t))
-        else
-            print_r_cache[tostring(t)] = true
-            if type(t) == "table" then
-                for i in t do
-                    local val = t[i]
-                    if (type(val) == "table") then
-                        print(indent .. "#[" .. i .. "] => " .. tostring(t) .. " {")
-                        sub_print_r(val, indent .. string.rep(" ", string.len(i) + 8))
-                        print(indent .. string.rep(" ", string.len(i) + 6) .. "}")
-                    elseif (type(val) == "string") then
-                        print(indent .. "#[" .. i .. '] => "' .. val .. '"')
-                    else
-                        print(indent .. "#[" .. i .. "] => " .. tostring(val))
-                    end
-                end
-                for pos, val in pairs(t) do
-                    if type(pos) ~= "number" or math.floor(pos) ~= pos or (pos < 1 or pos > tLen) then
-                        if (type(val) == "table") then
-                            print(indent .. "[" .. pos .. "] => " .. tostring(t) .. " {")
-                            sub_print_r(val, indent .. string.rep(" ", string.len(pos) + 8))
-                            print(indent .. string.rep(" ", string.len(pos) + 6) .. "}")
-                        elseif (type(val) == "string") then
-                            print(indent .. "[" .. pos .. '] => "' .. val .. '"')
-                        else
-                            print(indent .. "[" .. pos .. "] => " .. tostring(val))
-                        end
-                    end
-                end
-            else
-                print(indent .. tostring(t))
-            end
-        end
-    end
-
-    if (type(t) == "table") then
-        print(tostring(t) .. " {")
-        sub_print_r(t, "  ")
-        print("}")
-    else
-        sub_print_r(t, "  ")
-    end
-
-    print()
-end
-
-local function Print(msg)
+local function Print()
     if not DEFAULT_CHAT_FRAME or not msg then
         return;
     end
-
+    if type(msg) == 'table' then return print_r(msg) end
     DEFAULT_CHAT_FRAME:AddMessage("Blinker " .. msg);
 end
+
 local function SpellId(spellname)
     local id = 1;
     for i = 1, GetNumSpellTabs() do
@@ -88,14 +38,6 @@ local function SpellReady(spellname)
     end
 end
 
-local function Status()
-    local txt = "";
-    local n = UnitXP("timer", "size");
-    if _timer_id then
-        txt = "id: " .. _timer_id;
-    end
-    Print("Status:" .. txt .. " Running: #" .. n);
-end
 local function IsMoving(x, y)
     if not _prev_x or not _prev_y then
         _prev_x, _prev_y = GetPlayerMapPosition("player");
@@ -145,7 +87,7 @@ function Blinker_UI()
     StaticPopupDialogs["BLINKER_UI_FRAME_DIALOG_SPELL"] = {
         text = "Enter spell name:",
         hasEditBox = true,
-        OnAccept = function(self)
+        OnAccept = function()
             if SpellId(StaticPopup1EditBox:GetText()) then
                 Blinker_Settings.spell_name = StaticPopup1EditBox:GetText()
                 TooltipFrame_Show(UIParent, "Successfully set to: " .. Blinker_Settings.spell_name)
@@ -178,14 +120,14 @@ function Blinker_UI()
     MinimapButtonFrame:SetNormalTexture("Interface\\Icons\\Spell_Arcane_Blink")
     MinimapButtonFrame:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
     MinimapButtonFrame:SetScript("OnEnter", function(self, event)
-        local str = ""
+        local status
         if _timer_id then
-            str = str .. "active"
+            status = "active"
         else
-            str = str .. "inactive"
+            status = "inactive"
         end
 
-        TooltipFrame_Show(MinimapButtonFrame, str .. ": " .. Blinker_Settings.spell_name)
+        TooltipFrame_Show(MinimapButtonFrame, status .. ": " .. Blinker_Settings.spell_name)
     end)
 
     MinimapButtonFrame:SetScript("OnClick", function(self)
@@ -220,28 +162,6 @@ function Blinker()
 end
 
 function Blinker_Enable()
-    if not _unitxp_loaded then
-        Print("cannot load. Dependency UnitXP_SP3 not found. [https://github.com/allfoxwy/UnitXP_SP3].");
-        return;
-    end
-
-    if not Blinker_Settings then
-        Blinker_Settings = {
-            spell_name = "Blink"
-        }
-    end
-
-    if _timer_id and _debug then
-        Status()
-    end
-
-    _last_spell_cast = GetTime()
-    _timer_id = UnitXP("timer", "arm", 100, 100, "Blinker");
-    if _debug then
-        Print("Blinker_Enable: id=" .. _timer_id);
-        Status()
-    end
-    Print("is enabled.")
 end
 function Blinker_Disable()
     if not _unitxp_loaded then
@@ -260,21 +180,82 @@ function Blinker_Disable()
     end
     Print("is disabled.")
 end
-function Blinker_OnLoad()
-    this:RegisterEvent("ADDON_LOADED")
-    this:RegisterEvent("PLAYER_LOGIN")
-    this:RegisterEvent("PLAYER_LOGOUT")
-end
-function Blinker_OnEvent(event)
-    if event == "ADDON_LOADED" and arg1 == "Blinker" then
-        if pcall(UnitXP, "nop", "nop") then
-            _unitxp_loaded = true
-        end
-        this:UnregisterEvent("ADDON_LOADED")
-    elseif event == "PLAYER_LOGIN" then
-        Blinker_Enable()
-        Blinker_UI()
-    elseif event == "PLAYER_LOGOUT" then
-        Blinker_Disable()
+
+
+UnitXPUtil = {
+    loaded = false,
+    timers = {}
+}
+function UnitXPUtil:new()
+    if pcall(UnitXP, "nop", "nop") then
+        self.loaded = true
     end
+
+    return setmetatable(UnitXPUtil, { __index = UnitXPUtil })
 end
+function UnitXPUtil:timer(id, ms)
+    if self.timers[id] then
+        UnitXP("timer", "disarm", id)
+    end
+    self.timers[id] = UnitXP("timer", "arm", ms, ms, id);
+    return self.timers[id]
+end
+function UnitXPUtil:disable(id)
+    if not self.timers[id] then
+        Printf()
+    end
+    UnitXP("timer", "disarm", self.timers[id])
+    self.timers[id] = nil
+end
+
+Blinker = {
+    UnitXP = UnitXPUtil:new(),
+    Frame = nil,
+    enabled = false, debug = false,
+    prev_x = nil, prev_y = nil, 
+    last_spell_cast = GetTime(),
+
+    TooltipFrame = nil,
+    MinimapFrame = nil,
+    MinimapButtonFrame = nil,
+}
+
+function Blinker:new()
+    return setmetatable(Blinker, { __index = Blinker })
+end
+function Blinker:init()
+    self.Frame = CreateFrame("Frame", "BLINKER_UI_FRAME")
+    self.Frame:RegisterEvent("ADDON_LOADED")
+    self.Frame:RegisterEvent("PLAYER_LOGIN")
+    self.Frame:RegisterEvent("PLAYER_LOGOUT")
+    self.Frame:SetScript('OnEvent', function ()
+        if event == "ADDON_LOADED" and arg1 == "Blinker" then
+            if not Blinker_Settings then
+                Blinker_Settings = {
+                    spell_name = "Blink"
+                }
+            end
+            this:UnregisterEvent("ADDON_LOADED")
+        elseif event == "PLAYER_LOGIN" then
+            self:enable()
+        elseif event == "PLAYER_LOGOUT" then
+            self:disable()
+        end
+    end)
+end
+function Blinker:enable()
+    if not self.UnitXP.loaded then
+        Print("cannot load. Dependency UnitXP_SP3 not found. [https://github.com/allfoxwy/UnitXP_SP3].");
+        return;
+    end
+
+    self.UnitXP:timer("Blinker", 100);
+    Print("is enabled.")
+end
+function Blinker:disable()
+    self.UnitXP:disable("Blinker")
+    Print('Disabled.')
+end
+
+Blinker = Blinker:new()
+Blinker:init()
